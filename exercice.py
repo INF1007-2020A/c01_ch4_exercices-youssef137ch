084174c3dc1f1257b8026a79d3c1ea5bccfe80a3
youssef137ch <youssef.chourifi@polymtl.ca>
2020-09-17 11:05:29 -0400

    working exercice

--- exercice.py
+++ exercice.py
@@ -19,7 +19,7 @@ def get_nb_char(string: str, char: str) -> int:
 
 
 def get_nb_words(sentence: str) -> int:
-    return len(sentence.split())
+    return len(sentence.split(" "))
 
 
 def main() -> None:
